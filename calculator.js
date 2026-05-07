12282e5a1a624d9b79e37d8e96b92849c8930560
barackyusuff-crypto <barackyusuff@gmail.com>
2026-05-07 11:55:26 +0200

    neww

---
+++ calculator.js
@@ -0,0 +1,43 @@
+/**
+ * calculate the weighted average
+ * weighted :each grade is multiplied by it's credit weighted
+ * formula : sum(grade * credits) / sum(credits)
+ */
+
+function calculateWeightedAverage(subjectList) {
+    if( subjectList.length === 0) return 0;
+
+    const totalWeighted = subjectList.reduce(function(sum, s) {
+        return sum + (s.grade * s.credits) 
+    }, 0);
+
+    const totalCredits = subjectList.reduce(function(sum, s){
+        return sum + s.credits
+    }, 0);
+
+    return ( totalWeighted / totalCredits).toFixed(2);
+}
+
+// convert percentage  to GPA
+
+function calculateGpa(average){
+    const avg = parseFloat(average);
+    if (avg > 90) return 4.0;
+    if (avg > 85) return 3.7;
+    if (avg > 80) return 3.3;
+    if (avg > 75) return 3.0;
+    if (avg > 70) return 2.7;
+    if (avg > 65) return 2.3;
+    if (avg > 60) return 2.0;
+    return 0.0;
+}
+
+// 1. render results to DOM
+
+function renderCalculator() {
+    
+
+}
+// 2. update text element
+// 3. update animated progress bar
+// 4. update the home page stats too 
